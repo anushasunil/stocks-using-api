b2b6e01e797ea13b9da8ef30dc43097db2ce1b66
Anusha <anushasunil71201@gmail.com>
2021-10-02 19:37:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var initialPrice = document.querySelector("initial-price");
+var currentPrice = document.querySelector("current-price");
+var quantity = document.querySelector("quantity");
+
+var btnCheck = document.querySelector("btn-check");
